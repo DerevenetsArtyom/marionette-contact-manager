8371be78b296d164f591527d337785e310510851
DerevenetsArtyom <artem@comvoy.be>
2018-05-13 12:48:42 +0300

    Add 'serializeData' to ContactForm view

--- app/js/views/contactForm.js
+++ app/js/views/contactForm.js
@@ -1,25 +1,24 @@
-ContactManager.Views.ContactForm = Backbone.View.extend({
-  template: _.template($('#tpl-new-contact').html()),
+ContactManager.Views.ContactForm = Marionette.ItemView.extend({
+    template: '#tpl-new-contact',
 
-  events: {
-    'submit .contract-form': 'onFormSubmit'
-  },
+    events: {
+        'submit .contract-form': 'onFormSubmit'
+    },
 
-  render: function() {
-    var html = this.template(_.extend(this.model.toJSON(), {
-      isNew: this.model.isNew()
-    }));
-    this.$el.append(html);
-    return this;
-  },
+    serializeData: function () {
+        return _.extend(this.model.toJSON(), {
+            isNew: this.model.isNew()
+        })
+    },
+    
 
-  onFormSubmit: function(e) {
-    e.preventDefault();
+    onFormSubmit: function(e) {
+        e.preventDefault();
 
-    this.trigger('form:submitted', {
-      name: this.$('.contact-name-input').val(),
-      tel: this.$('.contact-tel-input').val(),
-      email: this.$('.contact-email-input').val()
-    });
-  }
+        this.trigger('form:submitted', {
+            name: this.$('.contact-name-input').val(),
+            tel: this.$('.contact-tel-input').val(),
+            email: this.$('.contact-email-input').val()
+        });
+    }
 });
