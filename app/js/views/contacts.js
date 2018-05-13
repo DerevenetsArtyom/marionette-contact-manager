49d6247bb2b6a4b54fec3b30a07dcaec199ba668
DerevenetsArtyom <artem@comvoy.be>
2018-05-13 18:48:10 +0300

    Add 'triggers' to Contacts view

--- app/js/views/contacts.js
+++ app/js/views/contacts.js
@@ -2,5 +2,9 @@ ContactManager.Views.Contacts = Marionette.CompositeView.extend({
     template: '#tpl-contacts',
 
     itemView: ContactManager.Views.Contact,
-    itemViewContainer: '.contacts-container'
+    itemViewContainer: '.contacts-container',
+
+    triggers: {
+        'click .add-contact-btn': 'addContact:clicked'
+    }
 });
