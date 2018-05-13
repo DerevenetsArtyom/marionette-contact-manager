25968b67cc81f29a510d5fede83a29e77ba4b615
DerevenetsArtyom <artem@comvoy.be>
2018-05-13 13:00:18 +0300

    Add '.ui' to organize UI elements in ContactForm view

--- app/js/views/contactForm.js
+++ app/js/views/contactForm.js
@@ -5,20 +5,25 @@ ContactManager.Views.ContactForm = Marionette.ItemView.extend({
         'submit .contract-form': 'onFormSubmit'
     },
 
+    ui: {
+        nameInput: '.contact-name-input',
+        telInput: '.contact-tel-input',
+        emailInput: '.contact-email-input'
+    },
+
     serializeData: function () {
         return _.extend(this.model.toJSON(), {
             isNew: this.model.isNew()
         })
     },
-    
 
     onFormSubmit: function(e) {
         e.preventDefault();
 
         this.trigger('form:submitted', {
-            name: this.$('.contact-name-input').val(),
-            tel: this.$('.contact-tel-input').val(),
-            email: this.$('.contact-email-input').val()
+            name: this.ui.nameInput.val(),
+            tel: this.ui.telInput.val(),
+            email: this.ui.emailInput.val()
         });
     }
 });
